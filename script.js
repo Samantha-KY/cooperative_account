00b1408dcf66ae79d097715f1204ed060f23f5df
Samiskina <mureraksamantha@gmail.com>
2022-09-03 12:02:43 +0200

    09_02

---
+++ script.js
@@ -0,0 +1,13 @@
+// const fetch = require('node-fetch');
+
+// fetch('http://localhost:2000/members').then((data) => {
+//     return data.json();
+// }).then((ObjectData) => {
+//     console.log(ObjectData[0].first_name);
+//     let tableData = "";
+// ObjectData.map((values) => {
+//     tableData+=`<h1>${values.member_id}</h1>`;
+// });
+// document.getElementById("table_body").innerHTML = tableData;
+
+// })
