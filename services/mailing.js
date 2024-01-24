74b3a99418c5f714270e07efb0f158c104d7abcd
Samantha-KY <mureraksamantha@gmail.com>
2024-01-24 18:21:58 +0200

    feat: change messae

--- services/mailing.js
+++ services/mailing.js
@@ -1,33 +1,32 @@
-const nodemailer = require('nodemailer')
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'mureraksamantha@gmail.com',
-        pass: 'sam2000'
-    }
+  service: "gmail",
+  auth: {
+    user: "mureraksamantha@gmail.com",
+    pass: "sam2000",
+  },
 });
 
 transporter.verify(function (error, success) {
-    if(error) {
-        console.log(error);
-    } else {
-        console.log('Server validation done and ready for messages.')
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server validation done and ready for messages.");
+  }
 });
 
 const email = {
-    from: 'mureraksamantha@gmail.com',
-    to: 'mureraksamantha@gmail.com',
-    subject: 'Sending A Simple Email using Node.js',
-    text: 'Now is the time for all good men to send Email via Node.js!'
+  from: "mureraksamantha@gmail.com",
+  to: "mureraksamantha@gmail.com",
+  subject: "Sending A Simple Email using Node.js",
+  text: "Now is the time for all good men to send Email via Node.js!",
 };
 
-transporter.sendMail(email, function(error, success){
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Nodemailer Email sent: ' + success.response);
-    }
+transporter.sendMail(email, function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email sent: " + success.response);
+  }
 });
-
